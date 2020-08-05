25ec9fbfa7b7a98c30d19a5a76d2138f76eea7eb
Paulo Werneck <paulo.werneck@powerofdata.com.br>
2020-08-05 15:21:52 -0300

    inserido script para novas tabelas do athena

---
+++ athena/create_table_stage_tb_crediario_cdc_output.sql
@@ -0,0 +1,21 @@
+----------------------------------------------------------------------------
+-- Autor: Paulo Werneck
+-- Data: 05/08/2020
+-- Desc: DDL - Tabela do AWS Athena com as respostas do dia corrente da api do modelo crediario_cdc
+----------------------------------------------------------------------------
+
+CREATE EXTERNAL TABLE TURING_PRD_SCOREAPI_USAGE.STAGE_TB_CREDIARIO_CDC_OUTPUT
+(
+	ID_TRANSACAO    STRING,
+	CD_CLI          BIGINT,
+	DATA            STRING,
+	SEGMENTO        STRING,
+	HIST_VV         DOUBLE,
+	SCORE           DOUBLE,
+    DT_PARTICAO     BIGINT
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION 's3://bkt-api-viavarejo-prd/report/stage/crediario_cdc/output/'
+TBLPROPERTIES("skip.header.line.count"="1");
